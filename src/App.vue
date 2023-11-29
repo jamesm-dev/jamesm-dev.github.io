eb4226ecea621f5c6c40eb8e6c8d4cba0b63a093
james <james@webpuppies.com.sg>
2023-11-30 04:12:16 +0800

    feat: add initial blocks

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,110 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive } from 'vue'
+import Block from '@/components/Block.vue'
 
-<template><h1 class="text-3xl font-bold underline">Hello world!</h1></template>
+const state = reactive({
+  focusedBlockIndex: -1
+})
+
+const setFocused = (blockIndex: number = -1) => {
+  state.focusedBlockIndex = blockIndex
+}
+
+const checkFocused = (blockIndex: number) =>
+  state.focusedBlockIndex === -1 || state.focusedBlockIndex === blockIndex
+</script>
+
+<template>
+  <div class="container mx-auto">
+    <div class="grid grid-rows-6 grid-cols-3 gap-6 min-h-screen p-6">
+      <Block
+        class="row-span-1 col-span-2"
+        blockLabel="Block 00"
+        :isBlur="!checkFocused(0)"
+        @mouseover="setFocused(0)"
+        @mouseleave="setFocused()"
+      />
+
+      <Block
+        class="row-span-2 col-span-1"
+        blockColor="#E4E2DF"
+        blockLabel="Webpuppies 2023"
+        blockTitle="Visa: Command Center"
+        blockText="Crisis management system for Visa Riyadh"
+        :isBlur="!checkFocused(1)"
+        @mouseover="setFocused(1)"
+        @mouseleave="setFocused()"
+      />
+
+      <Block
+        class="row-span-1 col-span-1"
+        blockColor="#E0DAFF"
+        blockLabel="Webpuppies 2023/Present"
+        blockTitle="Full-Stack Engineer"
+        blockText="Currenty part of Webpuppies Digital development team"
+        :isBlur="!checkFocused(2)"
+        @mouseover="setFocused(2)"
+        @mouseleave="setFocused()"
+      />
+
+      <Block
+        class="row-span-3 col-span-1"
+        blockLink="https://twitter.com/bushidoproj"
+        blockLabel="Personal"
+        blockTitle="Bushido Project"
+        blockText="7,777 female samurai NFT collection in Hedera Hashgraph"
+        :isBlur="!checkFocused(3)"
+        @mouseover="setFocused(3)"
+        @mouseleave="setFocused()"
+      />
+
+      <Block
+        class="row-span-2 col-span-1"
+        blockLink="https://cloudica.com"
+        blockLabel="Eversun 2021/2022"
+        blockTitle="Cloudica"
+        blockText="Live monitoring software for remote workers"
+        :isBlur="!checkFocused(4)"
+        @mouseover="setFocused(4)"
+        @mouseleave="setFocused()"
+      />
+
+      <Block
+        class="row-span-1 col-span-1"
+        blockColor="#FFD8D1"
+        blockLabel="Others 2019"
+        blockTitle="N4 Japanese Certification"
+        blockText="Passed both JLPT and JTEST japanese language exams"
+        :isBlur="!checkFocused(5)"
+        @mouseover="setFocused(5)"
+        @mouseleave="setFocused()"
+      />
+
+      <Block
+        class="row-span-3 col-span-1"
+        blockLabel="Block 06"
+        :isBlur="!checkFocused(6)"
+        @mouseover="setFocused(6)"
+        @mouseleave="setFocused()"
+      />
+
+      <Block
+        class="row-span-2 col-span-1"
+        blockLabel="Block 07"
+        :isBlur="!checkFocused(7)"
+        @mouseover="setFocused(7)"
+        @mouseleave="setFocused()"
+      />
+
+      <Block
+        class="row-span-2 col-span-1"
+        blockLabel="Block 08"
+        :isBlur="!checkFocused(8)"
+        @mouseover="setFocused(8)"
+        @mouseleave="setFocused()"
+      />
+    </div>
+  </div>
+</template>
 
 <style scoped></style>
---
+++ src/components/Block.vue
@@ -0,0 +1,124 @@
+<script setup lang="ts">
+import IconOpen from '@/components/icons/IconOpen.vue'
+
+defineProps({
+  class: {
+    type: String,
+    required: false
+  },
+  blockLink: {
+    type: String,
+    required: false
+  },
+  blockBackground: {
+    type: String,
+    required: false
+  },
+  blockColor: {
+    type: String,
+    default: '#E4E2DF'
+  },
+  blockLogo: {
+    type: String,
+    required: false
+  },
+  blockLabel: {
+    type: String,
+    required: false
+  },
+  blockTitle: {
+    type: String,
+    default: 'Block title'
+  },
+  blockText: {
+    type: String,
+    default: 'Block text'
+  },
+  isBlur: {
+    type: Boolean,
+    default: false
+  },
+  isDark: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const openLink = (url: string | null) => {
+  if (/^(ftp|http|https):\/\/[^ "]+$/i.test(String(url))) window.open(String(url), '_blank')
+}
+</script>
+
+<template>
+  <div
+    class="group block"
+    :class="class"
+    :style="`background-color: ${blockColor}; opacity: ${isBlur ? 0.5 : 1}`"
+    @click="openLink(blockLink || null)"
+  >
+    <span v-if="blockLabel" class="label">{{ blockLabel }}</span>
+
+    <img v-if="blockLogo" :src="blockLogo" class="logo" />
+
+    <span
+      class="title"
+      :class="{ 'mt-auto': Boolean(!blockLogo) }"
+      :style="`color: ${isDark ? '#fff' : '#161616'}`"
+    >
+      {{ blockTitle }}
+    </span>
+
+    <span class="description" :style="`color: ${isDark ? '#fff' : '#161616'}`">
+      {{ blockText }}
+    </span>
+
+    <span v-if="Boolean(blockLink)" class="link opacity-0 group-hover:opacity-100">
+      <IconOpen class="icon" />
+    </span>
+  </div>
+</template>
+
+<style scoped>
+.icon {
+  width: 24px;
+  height: 24px;
+}
+
+.block {
+  height: 100%;
+  transition: opacity 200ms ease-in-out;
+
+  @apply relative flex flex-col box-border p-5 rounded-xl select-none;
+
+  .logo {
+    width: 40px;
+    min-width: 40px;
+    max-width: 40px;
+    height: 40px;
+    min-height: 40px;
+    max-height: 40px;
+
+    @apply mt-auto mb-2 rounded;
+  }
+
+  .link {
+    transition: opacity 200ms ease-in-out;
+
+    @apply absolute top-3 right-3 bg-white p-2.5 rounded-xl;
+  }
+
+  .label {
+    color: '#161616';
+
+    @apply bg-white/50 w-fit font-mono text-sm tracking-tighter px-2.5 py-0.5 rounded-md;
+  }
+
+  .title {
+    @apply font-semibold text-2xl;
+  }
+
+  .description {
+    @apply text-sm tracking-wide mt-1;
+  }
+}
+</style>
